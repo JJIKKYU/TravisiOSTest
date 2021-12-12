d6bb7a145349f48fa0dc7990c53617562f680b3e
Jin Gyun, Jeong <jjinggu96@gmail.com>
2021-12-12 15:39:29 +0900

    UPDATE : Swift Package Test

---
+++ Tests/TravisiOSTestTests/TravisiOSTestTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import TravisiOSTest
+
+final class TravisiOSTestTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(TravisiOSTest().text, "Hello, World!")
+    }
+}
